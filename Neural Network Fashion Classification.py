7c4e8ceacb3ced00f8e09aea94590f5770257bba
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-07-20 11:01:19 -0400

    Setting up the model

--- Neural Network Fashion Classification.py
+++ Neural Network Fashion Classification.py
@@ -14,9 +14,18 @@ class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
 train_images = train_images / 255.0
 test_images = test_images / 255.0
 
-print(train_images[7])
+'''print(train_images[7])
 
 plt.imshow(train_images[7], cmap=plt.cm.binary)
-plt.show()
+plt.show()'''
 
 #input layer has 28x28 = 784 neurons
+#output layer has 10 neurons (0-9), each representing a different class of fashion item
+
+model = keras.Sequential([
+    keras.layers.Flatten(input_shape=(28,28)),
+    keras.layers.Dense(128, activation='relu'),
+    keras.layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', Loss='sparse_categorical_crossentropy', metrics=['accuracy'])
