3f9aaf6b0b528e6c8da4fe128f9258cff78c64a6
sameraslan <82460915+sameraslan@users.noreply.github.com>
2021-07-21 10:49:16 -0400

    Created for loop to show predicted and actual

--- Neural Network Fashion Classification.py
+++ Neural Network Fashion Classification.py
@@ -29,8 +29,20 @@ model = keras.Sequential([
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-model.fit(train_images, train_labels, epochs=20)
+model.fit(train_images, train_labels, epochs=5)
 test_loss, test_acc = model.evaluate(test_images, test_labels)
 
 # 5 epochs results in 87% accuracy and 20 epochs in 89%
-print("Tested: ", test_acc)
+#print("Tested: ", test_acc)
+
+prediction = model.predict(test_images)
+
+for i in range(5):
+    plt.grid(False)
+    plt.imshow(test_images[i], cmap=plt.cm.binary)
+    plt.xlabel("Actual: ", test_labels[i])
+    plt.title("Prediction: ", class_names[np.argmax(prediction[i])])
+    plt.show()
+    
+#print(prediction[0])
+#print(class_names[np.argmax(prediction[0])])
